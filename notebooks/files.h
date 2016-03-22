37a9840df3b04d709b7263bf75f3e11e4e65e66f
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2016-03-22 15:50:13 +0100

    Update of test notebook: dependency of file.h

---
+++ notebooks/files.h
@@ -0,0 +1,12 @@
+
+TChain *InputDataEgamma = new TChain("mini");
+TChain *InputDataMuons  = new TChain("mini");
+TChain *InputMonteCarlo = new TChain("mini");
+
+//InputDataEgamma->Add("/Users/clitemnestra/Downloads/DataEgammaA.root");
+
+
+//InputDataEgamma->Add("http://arturos.web.cern.ch/arturos/napoli/OutReach/data/DataEgammaA.root");
+//InputDataEgamma->Add("http://arturos.web.cern.ch/arturos/napoli/OutReach/data/DataEgammaB.root");
+//InputDataEgamma->Add("http://arturos.web.cern.ch/arturos/napoli/OutReach/data/DataEgammaC.root");
+
